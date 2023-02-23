d7494431c8300084e81a11b5530bee1a40a56251
Juan <juhernandezm@unal.edu.co>
2023-02-23 17:11:56 -0500

    count unreaded notifications listo

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const notifs= document.querySelectorAll('.notif')
 const unreadDots= document.querySelectorAll('.notif__redDot')
 const readBtn=document.querySelector('.readButton') 
+const topNumber=document.querySelector('.up__unreadnum')
 
 // LOGICA PARA APAGAR TODOS DE UNA SOLA VEZ!
 readBtn.addEventListener('click', ()=>{
     notifs.forEach((notif)=>{
         let isRead= notif.classList.contains('readed')
         if(!isRead){
-            notif.classList.toggle('readed')
+            notif.classList.add('readed')
         }
     })
     unreadDots.forEach((dot)=>{
         let isntDotted=dot.classList.contains('inactive')
         if(!isntDotted){
-            dot.classList.toggle('inactive')
+            dot.classList.add('inactive')
         }
     })
+    howManyUnreads()
 })
+// CONTAR CUANTOS UNREAD HAY
+function howManyUnreads(){
+    let unreadedNotifications=0
+    notifs.forEach((notif)=>{
+        let isRead=notif.classList.contains('readed')
+        if(!isRead){
+            unreadedNotifications+=1
+        }
+    })
+    topNumber.innerHTML=unreadedNotifications
+}
